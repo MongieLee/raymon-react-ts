2b06f14eed1800e8a0e74a3a6f2debf0c09ded26
MongieLee <le30n@outlook.com>
2022-02-17 10:45:46 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import {ProjectListScreen} from "./screens/project-list";
 
+console.log(process.env.NOVE_ENV);
+
 function App() {
   return (
     <div className="App">
