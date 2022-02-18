0d9c63b9fdd06f10cdfd6843e92bbf3a6db676a1
MongieLee <le30n@outlook.com>
2022-02-18 15:24:00 +0800

    feat: 安装Emotion，优化登录注册界面

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,17 +1,74 @@
 import React, {useState} from "react";
 import {RegisterScreen} from "./register";
 import {LoginScreen} from "./login";
-import {Card} from "antd";
+import {Button, Card, Divider} from "antd";
+import styled from "@emotion/styled";
+import logo from "assets/logo.svg";
+import left from "assets/left.svg";
+import right from "assets/right.svg";
 
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
 
   return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-      <Card>
+    <Container>
+      <Header/>
+      <Background/>
+      <ShadowCard>
+        <Title>
+          {isRegister ? "请注册" : "请登录"}
+        </Title>
         {isRegister ? <RegisterScreen/> : <LoginScreen/>}
-        <button onClick={() => setIsRegister(!isRegister)}>切换到{isRegister ? "登录" : "注册"}</button>
-      </Card>
-    </div>
+        <Divider/>
+        <a onClick={() => setIsRegister(!isRegister)}>
+          {isRegister ? "已经有账号了？立即去登录" : "没有账号？注册新账号"}
+        </a>
+      </ShadowCard>
+    </Container>
   );
 };
+
+const Title = styled.h2`
+  margin-botom: 2.4rem;
+  color: rgb(97, 104, 115);
+`;
+
+const Background = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: left bottom, right bottom;
+  background-size: calc(((100vw - 40rem) / 2) - 3.2rem), calc(((100vw - 40rem) / 2) - 3.2rem), cover;
+  background-image: url(${left}), url(${right});
+  // 40rem是中间表单区域的宽度  
+`;
+
+const Header = styled.header`
+  background: url(${logo}) no-repeat center;
+  padding: 5rem 0;
+  background-size: 8rem;
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+export const LongButton = styled(Button)`
+  width: 100%;
+`;
+
+const ShadowCard = styled(Card)`
+  width: 40rem;
+  min-height: 56rem;
+  padding: 3.2rem;
+  border-sizing: border-box;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
